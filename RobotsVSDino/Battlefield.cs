1915341d6b3dd5660361f6a28bce6b2eeadd85dd
Kevin Harvey Jr <lordty@gmail.com>
2020-08-11 20:49:25 -0500

    Finished_Battle_Logic

--- RobotsVSDino/Battlefield.cs
+++ RobotsVSDino/Battlefield.cs
@@ -27,17 +27,28 @@ namespace RobotsVSDino
                 if (herd.dinosaurs[0].dinoHealth <= 0)
                 {
                     herd.dinosaurs.RemoveAt(0);
+                    if(herd.dinosaurs.Count == 0)
+                    {
+                        break;
+                    }
                 }
 
+
+                herd.dinosaurs[0].Attack(fleet.robots[0]);
                 if (herd.dinosaurs.Count > 0)
                 {
-                    herd.dinosaurs[0].Attack(fleet.robots[0]);
-
                     // if robot dies, remove it from list
                     fleet.robots.RemoveAt(0);
                 }
-                // else (fleet.robots[0] && herd.dinosaurs[0])    
 
+                if(fleet.robots.Count > 0)
+                {
+                    Console.WriteLine("The Robots from the future has won");
+                }
+                else
+                {
+                    Console.WriteLine("The Dinosaurs from the past has won");
+                }
             }
 
 
